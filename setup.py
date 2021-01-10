56c4e8bc5b4cd39d71053fb18000476255360a99
hsnkhaki <hsnkhaki@yahoo.com>
2021-01-10 15:45:32 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Hossein',                   # Type in your name
   author_email = 'hsnkhaki@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/hsnkhaki',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/hsnkhaki/python-ramzinex/archive/main.zip',    # I explain this later on
+  download_url = 'https://github.com/hsnkhaki/python-ramzinex/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['trade', 'bitcoin', 'automate'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
